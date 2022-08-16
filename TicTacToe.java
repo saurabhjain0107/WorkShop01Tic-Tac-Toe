63454fd20b358c2f2a9798a92a9e6b402b28da10
saurabh jain <luckyjain0107@gmai.com>
2022-08-16 19:34:48 +0530

    Show Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -23,11 +23,21 @@ public class TicTacToe {
         else
             return 'x';
     }
+    static void showBoard(char[] board){
+        for (int i = 1; i<board.length; i++){
+            System.out.print(board[i]);
+            System.out.print(" ");
+            if(i%3==0){
+                System.out.println();
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game");
         createBoard(board);
         System.out.println("Player Choice is "+ playerChoice());
         System.out.println("Computer Choice is "+ computerChoice());
+        showBoard(board);
     }
 
 }
