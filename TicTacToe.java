e01cf35b1fc4d992ae659d60bb5f065399961c64
saurabh jain <luckyjain0107@gmai.com>
2022-08-19 12:15:18 +0530

    Who start first

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,73 +3,75 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToe {
-    static char player;
-    static char computerPlayer;
-    static char[] board = new char[10];
-    static Scanner sc = new Scanner(System.in);
-    static char Symbol;
+    static final int PLAYER_1=0;    //user
+    static final int PLAYER_2=1;    //computer
+    static int currentPlayer;
+    static char player1letter=' ';
+    static char player2letter=' ';
+    static Scanner scanner=new Scanner(System.in);
+    static char []board=new char[]{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',};
 
-    static void createBoard(char[] board) {
-        for (int i = 1; i < board.length; i++) {
-            board[i] = '_';
+    public static void main(String[] args) {
+        int position;
+        System.out.println("Welcome to Tic Tac Toe Game");
+
+        player1letter=chooseLetter();
+        player2letter=(player1letter == 'O') ? 'X': 'O';
+
+        System.out.println("player1letter = "+player1letter);
+        System.out.println("player2letter = "+player2letter);
+
+        currentPlayer=doToss();
+        showBoard();
+
+        if (currentPlayer == PLAYER_1) {
+            System.out.println("user won the toss");
+            System.out.println("select position on board (1 to 9)");
+            position=scanner.nextInt();
+        }else {
+            System.out.println("computer won the toss");
+            position= (int) ((Math.random()*10+1)%9);
         }
+
+        selectPosition(position);
+
+        showBoard();
     }
 
-    static char playerChoice() {
-        System.out.println("Choose a symbol 'x' or 'o' ");
-        Symbol = sc.next().charAt(0);
-        return Symbol;
+    static char chooseLetter(){
+        System.out.println("choose letter : O or X");
+        return scanner.next().toUpperCase().charAt(0);
     }
-
-    static char computerChoice() {
-        if (Symbol == 'x') {
-            return 'o';
-        } else
-            return 'x';
-    }
-
-    static void showBoard(char[] board) {
-        for (int i = 1; i < board.length; i++) {
-            System.out.print(board[i]);
-            System.out.print(" ");
-            if (i % 3 == 0) {
+    static void showBoard(){
+        System.out.println("showing board :");
+        for (int i = 1; i < 10; i++) {
+            System.out.print(board[i]+" .");
+            if (i == 3 || i==6) {
                 System.out.println();
             }
         }
+        System.out.println();
     }
 
-    static void userMove() {
-        System.out.println("Choose an index from 1 to 9 to write 'X': ");
-        Scanner scanner = new Scanner(System.in);
-        int index = scanner.nextInt();
-        if (board[index] == ' ') {
-            board[index] = player;
-        } else {
-            System.out.println("   ****** Choose an empty place! ******   ");
-            System.out.println();
-            userMove();
+    static void selectPosition(int number){
+        int position=number;
 
+        if (board[position] == ' ') {
+            board[position] =player1letter;
+        }else {
+            while (board[position] != ' '){
+                System.out.println(position+ " is already taken");
+                position=scanner.nextInt();
+            }
+            board[position] =player1letter;
         }
     }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe game");
-        createBoard(board);
-        System.out.println("Player Choice is " + playerChoice());
-        System.out.println("Computer Choice is " + computerChoice());
-        showBoard(board);
-
-        if (player == 'X') {
-            computerPlayer = 'O';
-        } else {
-            computerPlayer = 'X';
+    static int doToss(){
+        if((Math.random()*10)%2 ==1){
+            return PLAYER_1;
+        }else{
+            return PLAYER_2;
         }
-        while (true) {
-            userMove();
-            showBoard(board);
-        }
-
     }
-
 }
 
